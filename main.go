d4666dc010a3bc80ac1584f007436d74c06200f4
zcky <zackyfirdaus934@gmail.com>
2025-07-28 19:30:30 +0700

    if, else if, else, switch, for basic

---
+++ main.go
@@ -0,0 +1,36 @@
+package main;
+
+import "fmt"
+
+var orang string
+
+func main(){
+
+	umur := 12;
+
+orang ="tono";
+if orang == "jaki"{
+ fmt.Println("halo",orang)
+}else if  orang == "tono" {
+
+fmt.Println("halo", orang);
+
+}else{
+
+fmt.Println("ngawur");
+}
+
+switch umur{
+case 20: 
+fmt.Println("umur",umur)
+case 12:
+fmt.Println("umur",umur);
+}
+
+for x:= 1; x<6; x++{
+
+	fmt.Println("loop ke:", x)
+}
+
+}
+
