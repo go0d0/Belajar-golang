fa5489826375c728ffd261f540d5836f4e635b40
zacky <zackyfirdaus934@gmail.com>
2025-07-29 11:21:44 +0700

    tahap 4 setengah jalan

--- main.go
+++ main.go
@@ -1,36 +1,61 @@
-package main;
+package main
 
 import "fmt"
 
 var orang string
 
-func main(){
-
-	umur := 12;
-
-orang ="tono";
-if orang == "jaki"{
- fmt.Println("halo",orang)
-}else if  orang == "tono" {
-
-fmt.Println("halo", orang);
-
-}else{
-
-fmt.Println("ngawur");
+func tes(nama string) string { //fungsi diluar main
+	if nama == "jokowi" {
+		return "halo " + nama
+	} //multiple return
+	return "anda siapa?"
 }
 
-switch umur{
-case 20: 
-fmt.Println("umur",umur)
-case 12:
-fmt.Println("umur",umur);
+func main() {
+
+	umur := 12 // variabe umur
+
+	orang = "tono" // variabel orang
+
+	//cek value orang == "jaki"  atau == "tono" atau yang lain
+	if orang == "jaki" {
+		fmt.Println("halo", orang)
+	} else if orang == "tono" {
+
+		fmt.Println("halo", orang)
+
+	} else {
+
+		fmt.Println("ngawur")
+	}
+
+	// pengecekan kondisi umur
+	switch umur {
+	case 20:
+		fmt.Println("umur", umur)
+	case 12:
+		fmt.Println("umur", umur)
+	}
+
+	for x := 1; x < 6; x++ { //looping sampai 5
+
+		fmt.Println("loop ke:", x)
+	}
+
+	for y := 2; y < 4; y++ { //tes break
+		if y == 3 {
+			break
+
+		}
+		fmt.Println("tes break:", y)
+	}
+
+	for a := 0; a < 5; a++ { // tes continue
+		if a == 1 {
+			continue
+		}
+		fmt.Println("nilai continue:", a)
+	}
+
+	fmt.Println(tes("jokowis")) //manggil fungsi tes
 }
-
-for x:= 1; x<6; x++{
-
-	fmt.Println("loop ke:", x)
-}
-
-}
-
